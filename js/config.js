0c9ee1d2f051dacf6630a0da0371c84dc0f3f609
CombatPlatypus <lukejunqueira@hotmail.com>
2026-08-06 22:50:02 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,9 +4,6 @@ export const CONFIG = {
 
     google: {
 
-        clientId:
-            "671842519306-an0e8ahhftppp3v0bshjdq5d384ab1b9.apps.googleusercontent.com", // ID DA API GERADO PELO GOOGLE
-
         sheetsApiKey:
             "AIzaSyCATqioFrLxHy6STfRks_WGqmg0ynXq5nY", // CHAVE DA API DO SHEETS
 
@@ -14,13 +11,6 @@ export const CONFIG = {
             "1IS4QDnMenXTi07FhiL-nFINrEjLppAfZhSlogZq22hY", // ID DA PLANILHA COM AS CONFIGURAÇÕES DO DASHBOARD
 
         linksSpreadsheetRange:
-            "Planilhas!A2:F", // DETERMINA OS CAMPOS ONDE SERÁ BUSCADO AS INFORMAÇÕES DOS LINKS
-
-        scopes: [
-            "openid",
-            "email",
-            "profile",
-            "https://www.googleapis.com/auth/drive.readonly"
-        ]
+            "Planilhas!A2:F" // DETERMINA OS CAMPOS ONDE SERÁ BUSCADO AS INFORMAÇÕES DOS LINKS
     }
 };
