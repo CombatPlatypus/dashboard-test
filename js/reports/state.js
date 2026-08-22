499ed8f63c12fef149099bbbd593ab3fc28280c6
CombatPlatypus <lukejunqueira@hotmail.com>
2026-08-21 21:38:58 -0300

    Update state.js

--- js/reports/state.js
+++ js/reports/state.js
@@ -61,7 +61,7 @@ const planningCollectionPoolFields =
 let nextPlanningLhId = 1;
 let nextPlanningToId = 1;
 
-const MINIMUM_PLANNING_LHS = 3;
+const MINIMUM_PLANNING_LHS = 4;
 const MINIMUM_PLANNING_TOS_PER_LH = 1;
 
 /* NORMALIZA UM TEXTO */
